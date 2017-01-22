76d231fcc05049ba154e88d8e211a10d385f73cf
ttchengab <ttchengab@connect.ust.hk>
2017-01-22 10:46:03 +0800

    Update lcd_typewriter.h

--- inc/libsc/lcd_typewriter.h
+++ inc/libsc/lcd_typewriter.h
@@ -1,101 +1,118 @@
 /*
- * lcd_typewriter.h
+ * main.cpp
  *
- * Author: Ming Tsang
+ * Author: Peter
  * Copyright (c) 2014-2015 HKUST SmartCar Team
  * Refer to LICENSE for details
  */
 
-#pragma once
+#include <cassert>
+#include <cstring>
+#include <stdlib.h>
+#include <libbase/k60/mcg.h>
+#include <libsc/system.h>
+#include <libsc/k60/ov7725.h>
+#include <libsc/futaba_s3010.h>
+#include <libsc/alternate_motor.h>
+#include <libsc/lcd_typewriter.h>
 
-#include <cstddef>
-#include <cstdint>
 
-#include "libbase/helper.h"
-#include "libbase/misc_types.h"
 
-#include "libsc/st7735r.h"
-
-namespace libsc
+namespace libbase
 {
+	namespace k60
+	{
 
-/**
- * Draw text on Lcd. Working only with the Lcd interface, this class is rather
- * hardware independent. Since we have no plan (and largely unnecessary based on
- * the goal of this library) to support more than 1 font, the font data is also
- * being stored in this class
- */
-class LcdTypewriter
+		Mcg::Config Mcg::GetMcgConfig()
+		{
+			Mcg::Config config;
+			config.external_oscillator_khz = 50000;
+			config.core_clock_khz = 150000;
+			return config;
+		}
+
+	}
+}
+
+using namespace libsc;
+using namespace libbase::k60;
+using namespace libsc::k60;
+
+int main(void)
 {
-public:
-	// Conditionally select Lcd implementation here. Should prevent working with
-	// the interface directly (performance)
-	typedef St7735r Lcd;
+	System::Init();
 
-	struct Config
-	{
-		Lcd *lcd = nullptr;
-		uint16_t text_color = 0xFFFF;
-		uint16_t bg_color = 0;
-		/**
-		 * Start a newline when no more horizontal space left. If set, a newline
-		 * is always preferred over a halfly printed character
-		 */
-		bool is_text_wrap = false;
-		/**
-		 * Clear remaining pixel to BG color for each line
-		 */
-		bool is_clear_line = true;
-	};
+	Ov7725::Config C;  //camera init;
+	C.id = 0;
+	C.w = 80;
+	C.h = 60;
+	Ov7725 cam(C);
 
-	explicit LcdTypewriter(const Config &config);
+	St7735r::Config s;
+	s.is_revert = false;
+	s.is_bgr = false; //screen init;
+	s.fps = 100;
+	St7735r screen(s);
+	Timer::TimerInt t=0;
 
-	void WriteChar(const char ch);
-	void WriteString(const char *str);
-	void WriteBuffer(const char *buf, const size_t length);
+	LcdTypewriter::Config typewriter_config;
+	LcdTypewriter typewriter(typewriter_config);
 
-	void SetTextColor(const uint16_t color)
-	{
-		m_fg_color = color;
+	FutabaS3010::Config servo_config;
+	servo_config.id = 0;
+	FutabaS3010 servo(servo_config);
+
+	AlternateMotor::Config motor_config;
+	motor_config.multiplier=100;
+	AlternateMotor motor(motor_config);
+
+	int v[15][80], index; //v for value
+	int areaDiff=0, orgAreaDiff;
+	double pControl, dControl, Kp, Kd;
+	Kp = 0.05;
+	Kd = 0.02;
+
+	cam.Start();
+	servo.SetDegree(9.0);
+
+	while (true){
+		while(t!=System::Time()){
+			t = System::Time();
+
+			if(t % 10 == 0){
+				screen.SetRegion(Lcd::Rect(0,0,80,60));
+				screen.FillBits(St7735r::kBlack,St7735r::kWhite,cam.LockBuffer(),8*cam.GetBufferSize());
+/*
+				orgAreaDiff = areaDiff;
+				areaDiff = 0;
+				for(int line=11; line<26; line++){
+					index=0;
+					for(int i=0; i<10 ; i++){
+						for(int j=7; j>=0; j--){
+							v[line][index++] = (*(cam.LockBuffer()+line+i) >> j) & 1;
+						}
+					}
+
+					for(int i=1;i<79;i++){
+						if(v[line][i-1]+v[line][i]+v[line][i+1] >= 2){
+							if(i<40) areaDiff++;
+							else areaDiff--;
+						}
+					}
+				}
+				pControl = areaDiff * Kp; //to be determined
+				dControl = (areaDiff-orgAreaDiff) * Kd; //to be determined
+				servo.SetDegree(9.0 + pControl + dControl);
+
+
+				motor.SetPower((10-abs(servo.GetDegree()-9))*10); //motor speed depends on servo's turning deg, to be determined(maybe pid)
+*/
+				cam.UnlockBuffer();
+			}
+		}
 	}
 
-	uint16_t GetTextColor()
-	{
-		return m_fg_color;
-	}
+	cam.Stop();
 
-	void SetBgColor(const uint16_t color)
-	{
-		m_bg_color = color;
-	}
-
-	uint16_t GetBgColor()
-	{
-		return m_bg_color;
-	}
-
-	static constexpr Uint GetFontW()
-	{
-		return kFontW;
-	}
-
-	static constexpr Uint GetFontH()
-	{
-		return kFontH;
-	}
-
-private:
-	static constexpr uint8_t kFontW = 8;
-	static constexpr uint8_t kFontH = 16;
-	static const Byte kFontData[];
-
-	void WriteOneLineBuffer(const char *buf, const size_t length);
-
-	Lcd *m_lcd;
-	uint16_t m_fg_color;
-	uint16_t m_bg_color;
-	bool m_is_text_wrap;
-	bool m_is_clear_line;
-};
-
+	return 0;
 }
